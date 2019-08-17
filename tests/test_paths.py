52af466681ab6921eb8074141fb2a681ccac8754
Chris Simpkins <git.simpkins@gmail.com>
2019-08-17 00:28:47 -0400

    add paths module tests

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -1,2 +1,15 @@
-def test_something():
-    return True
+import os
+
+from dehinter.paths import filepath_exists
+
+import pytest
+
+
+def test_filepath_exists_good_filepath():
+    good_path = os.path.join("tests", "test_files", "fonts", "Hack-Regular.ttf")
+    assert filepath_exists(good_path) is True
+
+
+def test_filepath_exists_bad_filepath():
+    bad_path = "bogus_file.txt"
+    assert filepath_exists(bad_path) is False
